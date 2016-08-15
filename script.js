f9b1955ed7048648ca6bfbfedb14fe3d7f0eb5ac
nimost1 <nilsmorkstene@gmail.com>
2016-08-15 13:11:21 +0200

    Kan jeg ikke få "write access". Da blir alt mye enklere.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
-  
-
+//document.ready trengs ikke hvis det ikke er noe som skal skje med en gang sida åpnes.
+$(".container").click(function () {
+    $("#header").animate({marginTop: 30px}, 1000);
 });
+//Dette burde funke hvis bare jeg hadde fått til script-tagen.
